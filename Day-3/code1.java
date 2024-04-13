9aa6b33490428e4b8e9c4ee6315090315fb0f20f
Shrishti Bansal <141552678+TechShri027@users.noreply.github.com>
2024-04-13 22:52:01 +0530

    Add files via upload

---
+++ Day-3/code1.java
@@ -0,0 +1,14 @@
+class A {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        int c = 50;
+        if (a > b && a > c) {
+            System.out.println("a is larger");
+        } else if (b > c) {
+            System.out.println("b is larger");
+        } else {
+            System.out.println("c is larger");
+        }
+    }
+}
