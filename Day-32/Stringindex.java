bed7432e45d25c349ecf559de55ea1495ba605d4
Shrishti Bansal <141552678+TechShri027@users.noreply.github.com>
2024-08-04 23:13:05 +0530

    Add files via upload

---
+++ Day-32/Stringindex.java
@@ -0,0 +1,9 @@
+class Stringindex{
+    public static void main(String[] args) {
+        String str="shree";
+    
+          int index=str.charAt(0);
+          System.out.println(index);
+        
+    }
+}
